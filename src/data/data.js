ab1c0bad53fb4824df76f77953bcdb4f6d911615
JubairShaik <jubairahmed3000@gmail.com>
2024-07-16 12:28:56 +0530

    yeah

---
+++ src/data/data.js
@@ -0,0 +1,64 @@
+import project1 from "../assets/hotel-kusum.jpeg";
+import project2 from "../assets/travel-clone.jpeg";
+import project3 from "../assets/weather.jpeg";
+import project4 from "../assets/social-clone.jpeg";
+import project6 from "../assets/MERN-expense.jpeg";
+import project7 from "../assets/ecommerce.jpeg";
+import project8 from "../assets/blog-app.png";
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://ik.imagekit.io/imgkitt/tr:w-800/hotel-kusum.jpeg?updatedAt=1683134012764',
+    title: "Hotel Website (Live)",
+    desc: "I made this hotel website for a client, using HTML, CSS, JavaScript and it is responsive. It is also live.",
+    link: "https://kusumpalace.com/",
+    git: "https://github.com/SandipNITA2025/Nepal-hotel-projrct",
+  },
+  {
+    id: 2,
+    img: project2,
+    title: "Tarvel Agency",
+    link: "https://travel-agency-vite.netlify.app",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://ik.imagekit.io/imgkitt/tr:w-800/weather.jpeg?updatedAt=1683134012969',
+    title: "Weather Web",
+    desc: "I developed a weather web application using React.js that utilizes the Fetch API and includes PWA (Progressive Web App) functionality.",
+    link: "https://weatherwebbappp.netlify.app/",
+    git: "https://github.com/SandipNITA2025/weather-app",
+  },
+  {
+    id: 4,
+    img: "https://ik.imagekit.io/imgkitt/nike.JPG?updatedAt=1683563311643",
+    title: "Nike Clone (NextJs)",
+    desc: "This Nike clone has been built with the help of Next.js and Tailwind CSS..",
+    link: "https://nike-clones.vercel.app/",
+    git: "",
+  },
+  {
+    id: 5,
+    img: project6,
+    title: "MERN Expense Manager",
+    link: "https://strange-sunbonnet-lamb.cyclic.app/",
+    git: "",
+  },
+  {
+    id: 6,
+    img: 'https://ik.imagekit.io/imgkitt/tr:w-800/ecommerce.jpeg?updatedAt=1683134011136',
+    title: "Ecommerce Clone",
+    desc: "This is an e-commerce web application built using React JS, SCSS, Redux Toolkit, and a Dummy JSON API. It includes search functionality, as well as the ability to add and remove items from a shopping cart.",
+    link: "https://redux-store-app.netlify.app/",
+    git: "https://github.com/SandipNITA2025/react-redux-ecommerce",
+  },
+  {
+    id: 7,
+    img: 'https://ik.imagekit.io/imgkitt/mobile.png?updatedAt=1685388082890',
+    title: "Blog Web (MERN Stack)",
+    desc: "This is a full-stack web application that uses ReactJS, Tailwind CSS, ExpressJS, and MongoDB. The application features user authentication and enables users to create, edit, and delete their own posts, as well as view posts created by others.",
+    link: "https://blog-web-app-delta.vercel.app/",
+    git: "https://github.com/SandipNITA2025/MERN-STACK---BLOG-POST-WEB",
+  },
+];
